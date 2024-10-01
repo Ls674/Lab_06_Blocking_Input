7a68ccfb934d93aa704ce7cc5e653b9d0d420643
Ls674 <lx674superz@gmail.com>
2024-10-01 12:58:38 -0400

    CtoF Done

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -1,4 +1,4 @@
-public class Main {
+public class HighorLow {
     public static void main(String[] args) {
 
     }
--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -1,4 +1,4 @@
-public class Main {
+public class RectangleInfo {
     public static void main(String[] args) {
 
     }
