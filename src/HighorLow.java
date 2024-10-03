2a3b43e738816656d202b2e85fbb9caa3ebeb0a7
Ls674 <lx674superz@gmail.com>
2024-10-03 12:33:57 -0400

    Finished lab

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -1,5 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.random.RandomGenerator;
+
 public class HighorLow {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+        boolean onMoney = false;
+
+
+       do {
+           int randNum = rand.nextInt(11);
+           System.out.println("Guess the number through 10: ");
+           int guess = in.nextInt();
+
+           if (guess > randNum) {
+               System.out.println("Your guess is higher than the number");
+           }
+           else if (guess < randNum) {
+               System.out.println("Your guess is lower than the number");
+           } else if (guess == randNum) {
+               System.out.println("Your guess is on the money");
+
+           }
+       } while (!onMoney);
 
     }
 }
